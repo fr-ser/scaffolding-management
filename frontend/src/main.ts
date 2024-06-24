d79ef0e9894909f6ab27cc1ff66c7a5b777a9e38
Daria Eliseeva <elisey422das@gmail.com>
2024-06-24 21:35:38 +0200

    setup prime vue

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,19 @@
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 
+import PrimeVue from 'primevue/config';
+import Lara from '@/presets/lara';
+
 import App from '@/App.vue'
 import '@/assets/main.css'
 import router from '@/router'
 
 const app = createApp(App)
+
+app.use(PrimeVue, {
+    unstyled: true,
+    pt: Lara
+});
 
 app.use(createPinia())
 app.use(router)
