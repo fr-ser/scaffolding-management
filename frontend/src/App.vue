5c300f32a6c8b093198372ff5c3cbef2d2078c21
Daria Eliseeva <elisey422das@gmail.com>
2024-06-30 19:24:19 +0200

    menu list and icons were added

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,37 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { RouterView } from 'vue-router';
+import Menubar from 'primevue/menubar';
 
 import NavigationBar from '@/components/NavigationBar.vue'
+
+import { ref } from "vue";
+
+const items = ref([
+  {
+    label: 'Kunden',
+    icon: 'pi pi-user'
+  }, {
+    label: 'Aufträge',
+    icon: 'pi pi-shopping-cart'
+  }, {
+    label: 'Übersicht',
+    icon: 'pi pi-search'
+  }, {
+    label: 'Dokumente',
+    icon: 'pi pi-file'
+  }, {
+    label: 'Artikel',
+    icon: 'pi pi-list'
+  },
+
+]);
 </script>
 
 <template>
-  <NavigationBar />
+  <!-- <NavigationBar /> -->
+  <div class="card">
+    <Menubar :model="items" />
+  </div>
   <RouterView />
 
 </template>
