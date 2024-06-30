463050d1dba54b2529a73f0e1f9a0995193269ae
Daria Eliseeva <elisey422das@gmail.com>
2024-06-30 19:38:57 +0200

    routes were added

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,19 +9,24 @@ import { ref } from "vue";
 const items = ref([
   {
     label: 'Kunden',
-    icon: 'pi pi-user'
+    icon: 'pi pi-user',
+    route: '/customers'
   }, {
     label: 'Aufträge',
-    icon: 'pi pi-shopping-cart'
+    icon: 'pi pi-shopping-cart',
+    route: '/order'
   }, {
     label: 'Übersicht',
-    icon: 'pi pi-search'
+    icon: 'pi pi-search',
+    route: '/orders'
   }, {
     label: 'Dokumente',
-    icon: 'pi pi-file'
+    icon: 'pi pi-file',
+    route: '/documents'
   }, {
     label: 'Artikel',
-    icon: 'pi pi-list'
+    icon: 'pi pi-list',
+    route: '/articles'
   },
 
 ]);
@@ -30,7 +35,17 @@ const items = ref([
 <template>
   <!-- <NavigationBar /> -->
   <div class="card">
-    <Menubar :model="items" />
+    <Menubar :model="items">
+      <template #item="{ item, props, hasSubmenu }">
+        <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
+          <a v-ripple :href="href" v-bind="props.action" @click="navigate">
+            <span :class="item.icon" />
+            <span class="ml-2">{{ item.label }}</span>
+          </a>
+        </router-link>
+
+      </template>
+    </Menubar>
   </div>
   <RouterView />
 
