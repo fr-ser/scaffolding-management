f752cdcb0f48868dee237fec38e68ab7d9899af0
Daria Eliseeva <elisey422das@gmail.com>
2024-07-01 13:22:35 +0200

    create search and new user button

--- frontend/src/views/ClientView.vue
+++ frontend/src/views/ClientView.vue
@@ -1,7 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+
+const value = ref(null);
+</script>
 
 <template>
-  <h1>I want to be a client page</h1>
+  <div class="card flex w-full gap-x-6">
+    <span class="relative grow">
+      <i class="pi pi-search absolute top-2/4 -mt-2 left-3 text-surface-400 dark:text-surface-600" />
+      <InputText v-model="value1" placeholder="Search for client" class="pl-10 w-full" />
+    </span>
+
+    <Button class="md:hidden" icon="pi pi-user" size="small" rounded aria-label="User" />
+    <Button class="hidden md:inline-block" label="Create new client" />
+
+    <!-- <InputText type="text" v-model="value" /> -->
+  </div>
 </template>
 
 <style></style>
