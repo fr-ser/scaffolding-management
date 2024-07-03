127b3c100b83c20cb942c651ea50726dfc06f7ce
Daria Eliseeva <elisey422das@gmail.com>
2024-07-03 15:02:56 +0200

    create cards

--- frontend/src/views/ClientView.vue
+++ frontend/src/views/ClientView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import Button from "primevue/button";
 import Calendar from "primevue/calendar";
 import Card from "primevue/card";
 import Dropdown from "primevue/dropdown";
@@ -33,88 +34,104 @@ const genders = ref([
 
 <template>
   <form>
-    <Card class="my-2">
-      <template #content>
-        <div>Name</div>
-        <div class="card flex flex-col justify-center gap-y-6">
-          <Dropdown
-            v-model="selectedGender"
-            :options="genders"
-            optionLabel="gender"
-            placeholder="Anrede"
-            class="w-full md:w-[14rem]"
-          />
-          <FloatLabel>
-            <InputText id="username" v-model="userInfo.name" class="w-full" />
-            <label for="username">Vorname</label>
-          </FloatLabel>
-          <FloatLabel>
-            <InputText id="usesurname" v-model="userInfo.surname" class="w-full" />
-            <label for="usersurname">Nachname</label>
-          </FloatLabel>
-        </div>
-      </template>
-    </Card>
-    <Card class="my-2">
-      <template #content>
-        <div>Adresse</div>
-        <div class="card flex flex-col justify-center gap-y-6">
-          <FloatLabel>
-            <InputText id="street" v-model="userInfo.street" class="w-full" />
-            <label for="street">Straße und Nr.</label>
-          </FloatLabel>
-
-          <FloatLabel>
-            <InputNumber id="plz" v-model="userInfo.plz" inputId="integeronly" class="w-full" />
-            <label for="plz">PLZ</label>
-          </FloatLabel>
-          <FloatLabel>
-            <InputText id="city" v-model="userInfo.city" class="w-full" />
-            <label for="city">Stadt</label>
-          </FloatLabel>
-        </div>
-      </template>
-    </Card>
-    <Card class="my-2">
-      <template #content>
-        <div>Kontakt</div>
-        <div class="card flex flex-col justify-center gap-y-6">
-          <FloatLabel>
-            <InputText id="landline" v-model="userInfo.landline" class="w-full" />
-            <label for="landline">Festnetz</label>
-          </FloatLabel>
-
-          <FloatLabel>
-            <InputNumber id="mobil" v-model="userInfo.mobil" class="w-full" />
-            <label for="mobil">Mobil</label>
-          </FloatLabel>
-          <FloatLabel>
-            <InputText id="email" v-model="userInfo.email" class="w-full" />
-            <label for="email">E-Mail</label>
-          </FloatLabel>
-        </div>
-      </template>
-    </Card>
-    <Card class="my-2">
-      <template #content>
-        <div>Sonstiges</div>
-        <div class="card flex flex-col justify-center gap-y-6">
-          <FloatLabel>
-            <Calendar
-              id="calendar"
-              v-model="userInfo.date"
-              dateFormat="dd/mm/yy"
-              showIcon
-              iconDisplay="input"
+    <div class="flex flex-row justify-between">
+      <Button label="See contracts" severity="secondary" text raised />
+      <div class="flex gap-x-2">
+        <Button label="Save" text raised />
+        <Button label="Delete" severity="danger" text raised />
+      </div>
+    </div>
+    <div class="grid grid-cols-1 sm:grid-cols-2 sm:gap-2 xl:grid-cols-4 xl:gap-4">
+      <Card class="my-2">
+        <template #content>
+          <div class="mb-4 font-bold">Name</div>
+          <div class="card flex flex-col justify-center gap-y-6">
+            <Dropdown
+              v-model="selectedGender"
+              :options="genders"
+              optionLabel="gender"
+              placeholder="Anrede"
+              class="w-full md:w-[14rem]"
             />
-            <label for="calendar">Geburtstag</label>
-          </FloatLabel>
-          <FloatLabel>
-            <Textarea id="text" v-model="userInfo.text" autoResize rows="5" cols="30" />
-            <label for="text">Kommentar</label>
-          </FloatLabel>
-        </div>
-      </template>
-    </Card>
+            <FloatLabel>
+              <InputText id="username" v-model="userInfo.name" class="w-full" />
+              <label for="username">Vorname</label>
+            </FloatLabel>
+            <FloatLabel>
+              <InputText id="usesurname" v-model="userInfo.surname" class="w-full" />
+              <label for="usersurname">Nachname</label>
+            </FloatLabel>
+          </div>
+        </template>
+      </Card>
+      <Card class="my-2">
+        <template #content>
+          <div class="mb-4 font-bold">Adresse</div>
+          <div class="card flex flex-col justify-center gap-y-6">
+            <FloatLabel>
+              <InputText id="street" v-model="userInfo.street" class="w-full" />
+              <label for="street">Straße und Nr.</label>
+            </FloatLabel>
+
+            <FloatLabel>
+              <InputNumber id="plz" v-model="userInfo.plz" inputId="integeronly" class="w-full" />
+              <label for="plz">PLZ</label>
+            </FloatLabel>
+            <FloatLabel>
+              <InputText id="city" v-model="userInfo.city" class="w-full" />
+              <label for="city font-bold">Stadt</label>
+            </FloatLabel>
+          </div>
+        </template>
+      </Card>
+      <Card class="my-2">
+        <template #content>
+          <div class="mb-4">Kontakt</div>
+          <div class="card flex flex-col justify-center gap-y-6">
+            <FloatLabel>
+              <InputText id="landline" v-model="userInfo.landline" class="w-full" />
+              <label for="landline">Festnetz</label>
+            </FloatLabel>
+
+            <FloatLabel>
+              <InputNumber id="mobil" v-model="userInfo.mobil" class="w-full" />
+              <label for="mobil">Mobil</label>
+            </FloatLabel>
+            <FloatLabel>
+              <InputText id="email" v-model="userInfo.email" class="w-full" />
+              <label for="email">E-Mail</label>
+            </FloatLabel>
+          </div>
+        </template>
+      </Card>
+      <Card class="my-2">
+        <template #content>
+          <div class="mb-4 font-bold">Sonstiges</div>
+          <div class="card flex flex-col justify-center gap-y-6">
+            <FloatLabel>
+              <Calendar
+                id="calendar"
+                v-model="userInfo.date"
+                dateFormat="dd/mm/yy"
+                showIcon
+                iconDisplay="input"
+              />
+              <label for="calendar">Geburtstag</label>
+            </FloatLabel>
+            <FloatLabel>
+              <Textarea
+                id="text"
+                v-model="userInfo.text"
+                class="w-full"
+                autoResize
+                rows="5"
+                cols="30"
+              />
+              <label for="text">Kommentar</label>
+            </FloatLabel>
+          </div>
+        </template>
+      </Card>
+    </div>
   </form>
 </template>
