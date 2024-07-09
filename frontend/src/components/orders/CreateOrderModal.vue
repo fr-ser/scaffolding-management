fb5431575b4ce4e0171c9b4f88c822e1d46c91fa
Daria Eliseeva <elisey422das@gmail.com>
2024-07-09 15:51:42 +0200

    add hide modal button

---
+++ frontend/src/components/orders/CreateOrderModal.vue
@@ -0,0 +1,10 @@
+<script setup>
+const emit = defineEmits(["close-modal"]);
+function onButtonClick() {
+  emit("close-modal");
+}
+</script>
+<template>
+  <div>Hello world!</div>
+  <button @click="onButtonClick">close</button>
+</template>
--- frontend/src/components/orders/CreateOrderModel.vue
+++
@@ -1,7 +0,0 @@
-<script setup>
-// defineProps('isVisible');
-// const emit = defineEmits(['remove-item'])
-</script>
-<template>
-  <div>Hello world!</div>
-</template>
