024f468c0aa4bdcc89bae3eb1170a502e0962966
Sergej Herbert <sergej.herbert@sennder.com>
2024-07-24 21:39:01 +0200

    feat: add https support

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
+import fs from "node:fs";
+import https from "node:https";
 import { AddressInfo } from "node:net";
 
-import { DB_PATH, PORT } from "@/config";
+import { DB_PATH, HTTPS_CA_PATH, HTTPS_CERT_PATH, HTTPS_KEY_PATH, PORT, USE_HTTPS } from "@/config";
 import { closeDatabase, initializeAppDataSource } from "@/db";
 import { log } from "@/helpers/logging";
 import { getApp } from "@/main";
@@ -12,9 +14,24 @@ async function main() {
   await initializeAppDataSource(DB_PATH);
   log(`Initialized database at ${DB_PATH}`);
 
-  const server = app.listen(PORT, () => {
-    log(`Node app started on port ${(server.address() as AddressInfo).port}`);
-  });
+  if (USE_HTTPS) {
+    const server = https
+      .createServer(
+        {
+          key: fs.readFileSync(HTTPS_KEY_PATH as string),
+          cert: fs.readFileSync(HTTPS_CERT_PATH as string),
+          ca: fs.readFileSync(HTTPS_CA_PATH as string),
+        },
+        app,
+      )
+      .listen(PORT, () => {
+        log(`Node app started on port ${(server.address() as AddressInfo).port} with https`);
+      });
+  } else {
+    const server = app.listen(PORT, () => {
+      log(`Node app started on port ${(server.address() as AddressInfo).port}`);
+    });
+  }
 }
 
 main().catch((err) => {
