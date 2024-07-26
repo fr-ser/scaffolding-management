2daf5b06b22b68b1d633f7a724d6734ea46d54c2
Sergej Herbert <sergej.herbert@sennder.com>
2024-07-26 20:16:47 +0200

    feat: add permissions

--- backend/src/helpers/roleManagement.ts
+++ backend/src/helpers/roleManagement.ts
@@ -1,5 +1,7 @@
 import express from "express";
+import basicAuth from "express-basic-auth";
 
+import { USERS } from "@/config";
 import { UserRole } from "@/global/types/backendTypes";
 
 type MiddleWare = (
@@ -14,6 +16,12 @@ interface AuthOptions {
   all?: true;
 }
 
+function getRoleByUser(userName: string): UserRole {
+  const foundUser = Object.entries(USERS).find(([name]) => name === userName);
+  if (!foundUser) throw new Error("User not found");
+  return foundUser[1].role;
+}
+
 /** This function generates an appropriate middleware based on the params
  * @param yes List of accepted roles
  * @param no List of not accepted roles
@@ -23,10 +31,22 @@ interface AuthOptions {
 export function checkAuth(options: { yes: UserRole[] }): MiddleWare;
 export function checkAuth(options: { no: UserRole[] }): MiddleWare;
 export function checkAuth(options: { all: true }): MiddleWare;
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function checkAuth(_: AuthOptions): MiddleWare {
-  return (_: express.Request, _2: express.Response, next: express.NextFunction) => {
-    // TODO: add auth checks with basic auth
-    next();
+export function checkAuth(options: AuthOptions): MiddleWare {
+  const { yes = [], no = [], all = false } = options ? options : { all: true };
+  let allowedList: UserRole[];
+
+  if (all) allowedList = Object.values(UserRole);
+  else if (yes.length > 0) allowedList = yes;
+  else if (no.length > 0) {
+    allowedList = Object.values(UserRole).filter((role) => !no.includes(role));
+  }
+
+  return (req, res, next) => {
+    const request = req as basicAuth.IBasicAuthedRequest;
+    const role = getRoleByUser(request.auth.user);
+
+    if (!allowedList.includes(role)) {
+      res.status(403).send("Forbidden");
+    } else next();
   };
 }
