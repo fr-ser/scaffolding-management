b36d44d30cdc63685311197075d0115b7a3e79fa
Daria Eliseeva <elisey422das@gmail.com>
2024-08-05 14:06:24 +0200

    fix changes

--- frontend/src/components/orders/OrderDocuments.vue
+++ frontend/src/components/orders/OrderDocuments.vue
@@ -12,50 +12,45 @@ import {
   type OverdueNoticeDocument,
 } from "@/global/types/entities";
 
-const route = useRoute();
+const props = defineProps<{
+  id: string;
+}>();
+
+// const route = useRoute();
 let visible = ref(false);
-let search = ref("");
+// let search = ref("");
 let documents = ref<(OfferDocument | OverdueNoticeDocument | InvoiceDocument)[]>([]);
 
 async function getDocuments() {
-  documents.value = await getDocumentsByOrder(route.params.id as string);
+  documents.value = await getDocumentsByOrder(props.id as string);
 }
 async function openDocumentsList() {
   visible.value = true;
   getDocuments();
 }
-
-const filteredDocuments = computed(() => {
-  if (!search.value.trim()) {
-    return documents.value;
-  }
-  const query = search.value.toLowerCase();
-
-  return documents.value.filter((document) => document.id.toLowerCase().includes(query));
-});
 </script>
 
 <template>
-  <div class="font-bold my-2">Documente</div>
+  <div class="font-bold my-2">Dokumente</div>
   <div>
     <Button
       @click="openDocumentsList"
-      label="Alle auftragsdocument(e) anzeigen"
+      label="Alle Auftragsdokumente anzeigen"
       severity="secondary"
       outlined
       size="small"
     />
   </div>
-  <Dialog v-model:visible="visible" modal header="Order documents" :style="{ width: '25rem' }">
-    <div class="card flex w-full gap-x-6 mb-3">
-      <span class="relative grow bg-zinc-100">
+  <Dialog v-model:visible="visible" modal header="Auftragsdokumente" :style="{ width: '25rem' }">
+    <!-- <div class="card flex w-full gap-x-6 mb-3"> -->
+    <!-- <span class="relative grow bg-zinc-100">
         <i
           class="pi pi-search absolute top-2/4 -mt-2 left-3 text-surface-400 dark:text-surface-600"
-        />
-        <InputText v-model="search" placeholder="Suche" class="pl-10 w-full" />
-      </span>
-    </div>
-    <div v-for="document in filteredDocuments" :key="document.id">
+        /> -->
+    <!-- <InputText v-model="search" placeholder="Suche" class="pl-10 w-full" /> -->
+    <!-- </span> -->
+    <!-- </div> -->
+    <div v-for="document in documents" :key="document.id">
       <div class="border border-slate-300 hover:border-primary ps-4 py-1">{{ document.id }}</div>
     </div>
   </Dialog>
