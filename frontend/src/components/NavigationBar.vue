a61a33c944bb0692ebe74639c13a7fffa599ab88
Sergej Herbert <sergej.herbert@sennder.com>
2024-08-06 20:37:00 +0200

    chore: investigate routing error label

--- frontend/src/components/NavigationBar.vue
+++ frontend/src/components/NavigationBar.vue
@@ -31,20 +31,25 @@ const items = ref([
 ]);
 </script>
 
+<!--
+The primevue menu causes error logs like these:
+Blocked aria-hidden on a <a> element because the element that just received focus must not be hidden from assistive technology users
+
+This seems in line with this issue:
+https://github.com/primefaces/primevue/issues/6103
+-->
 <template>
-  <div class="card">
-    <Menubar :model="items">
-      <template #item="{ item, props }">
-        <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
-          <a :href="href" v-bind="props.action" @click="navigate">
-            <span :class="item.icon" />
-            <span class="ml-2">{{ item.label }}</span>
-          </a>
-        </router-link>
-      </template>
-      <template #end
-        ><h1 class="font-semibold">{{ route.meta?.label }}</h1></template
-      >
-    </Menubar>
-  </div>
+  <Menubar :model="items">
+    <template #item="{ item, props }">
+      <RouterLink v-slot="{ href, navigate }" :to="item.route" custom>
+        <a :href="href" v-bind="props.action" @click="navigate">
+          <span :class="item.icon" />
+          <span class="ml-2">{{ item.label }}</span>
+        </a>
+      </RouterLink>
+    </template>
+    <template #end>
+      <h1 class="font-semibold">{{ route.meta?.label }}</h1>
+    </template>
+  </Menubar>
 </template>
