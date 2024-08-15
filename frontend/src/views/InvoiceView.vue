57858796d3cb61f8ce85fbfcf1e3edd3a3a9d420
Daria Eliseeva <elisey422das@gmail.com>
2024-08-15 17:04:44 +0200

    add card with calendars

--- frontend/src/views/InvoiceView.vue
+++ frontend/src/views/InvoiceView.vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
+import Button from "primevue/button";
+import Calendar from "primevue/calendar";
 import Card from "primevue/card";
+import Dropdown from "primevue/dropdown";
+import FloatLabel from "primevue/floatlabel";
 import { onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
 
 import { getOrder } from "@/backendClient";
+import { PaymentStatus } from "@/global/types/appTypes";
 import type { Order } from "@/global/types/entities";
 import { ROUTES } from "@/router";
 
 const route = useRoute();
 let orderInfo = ref<Order | undefined>();
+const invoiceType = Object.values(PaymentStatus);
+let paymentTarget = ref();
+let invoiceDate = ref();
+let invoiceStatus = PaymentStatus.initial;
 onMounted(async () => {
   orderInfo.value = await getOrder(route.params.order_id as string);
 });
@@ -33,6 +42,46 @@ onMounted(async () => {
       <div>
         <span class="font-bold">Skonto: </span>
         <span>{{ orderInfo?.can_have_cash_discount ? "Ja" : "Nein" }}</span>
+      </div>
+    </template>
+  </Card>
+  <Card class="my-2">
+    <template #content>
+      <div class="mb-4 font-bold">Rechnung:</div>
+      <FloatLabel class="my-6">
+        <Calendar
+          id="calendar"
+          v-model="invoiceDate"
+          dateFormat="dd/mm/yy"
+          showIcon
+          iconDisplay="input"
+        />
+        <label for="calendar"> Rechnungsdatum </label>
+      </FloatLabel>
+      <FloatLabel class="my-6">
+        <Calendar
+          id="offered-at-input"
+          v-model="paymentTarget"
+          dateFormat="dd/mm/yy"
+          showIcon
+          iconDisplay="input"
+        />
+        <label for="calendar"> Zalungsziel </label>
+      </FloatLabel>
+      <div class="mb-2">Zahlungstatus:</div>
+      <Dropdown
+        v-model="invoiceStatus"
+        :options="invoiceType"
+        placeholder="Anrede"
+        class="w-full md:w-[14rem] mb-3"
+      />
+    </template>
+  </Card>
+  <Card class="my-2">
+    <template #content>
+      <div class="flex flex-row justify-between">
+        <div class="mb-4 font-bold">Leistungsdatum:</div>
+        <Button icon="pi pi-plus" rounded outlined />
       </div>
     </template>
   </Card>
