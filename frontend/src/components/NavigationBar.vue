550f27e9f2ebe2d5b959e95b149eb14d53cc1c46
Sergej Herbert <sergej.herbert@sennder.com>
2024-08-18 19:52:59 +0200

    fix: menu aria hidden issue

--- frontend/src/components/NavigationBar.vue
+++ frontend/src/components/NavigationBar.vue
@@ -31,15 +31,17 @@ const items = ref([
 ]);
 </script>
 
-<!--
-The primevue menu causes error logs like these:
-Blocked aria-hidden on a <a> element because the element that just received focus must not be hidden from assistive technology users
-
-This seems in line with this issue:
-https://github.com/primefaces/primevue/issues/6103
--->
 <template>
-  <Menubar :model="items">
+  <Menubar
+    :model="items"
+    :pt="{
+      // fixes https://github.com/primefaces/primevue/issues/6141
+      // and https://github.com/primefaces/primevue/issues/6103
+      action: {
+        ariaHidden: false,
+      },
+    }"
+  >
     <template #item="{ item, props }">
       <RouterLink v-slot="{ href, navigate }" :to="item.route" custom>
         <a :href="href" v-bind="props.action" @click="navigate">
