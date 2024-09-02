cf75e4bce75979e416656964c0de1e1e329a3ee8
Daria Eliseeva <elisey422das@gmail.com>
2024-09-02 15:03:52 +0200

    add component to invoiceView

--- frontend/src/views/InvoiceView.vue
+++ frontend/src/views/InvoiceView.vue
@@ -11,6 +11,7 @@ import { computed, onMounted, ref, watch } from "vue";
 import { useRoute } from "vue-router";
 
 import { getOrder } from "@/backendClient";
+import OrderSummary from "@/components/orders/OrderSummary.vue";
 import SubOrderItem from "@/components/orders/SubOrderItem.vue";
 import { ArticleKind } from "@/global/types/appTypes";
 import { PaymentStatus } from "@/global/types/appTypes";
@@ -109,128 +110,109 @@ onMounted(async () => {
 });
 </script>
 <template>
-  <div class="flex flex-row justify-between">
-    <router-link :to="`${ROUTES.ORDER.path}/${route.params.order_id}/edit`">
-      <Button icon="pi pi-arrow-left" size="small" severity="secondary" text raised />
-    </router-link>
-    <div class="flex gap-x-2">
-      <Button label="Speichern" text raised />
-      <Button label="Löschen" severity="danger" text raised />
+  <div v-if="orderInfo">
+    <div class="flex flex-row justify-between">
+      <router-link :to="`${ROUTES.ORDER.path}/${route.params.order_id}/edit`">
+        <Button icon="pi pi-arrow-left" size="small" severity="secondary" text raised />
+      </router-link>
+      <div class="flex gap-x-2">
+        <Button label="Speichern" text raised />
+        <Button label="Löschen" severity="danger" text raised />
+      </div>
     </div>
-  </div>
-  <Card class="my-2">
-    <template #content>
-      <div class="mb-4 font-bold">Auftragsdaten</div>
-      <div><span class="font-bold"> Bauvorhaben: </span>{{ orderInfo?.title }}</div>
-      <div>
-        <span class="font-bold"> Auftrags-Nr.: </span>
-        {{ orderInfo?.id }}
-      </div>
-      <div><span class="font-bold">Status: </span> {{ orderInfo?.status }}</div>
-      <div>
-        <span class="font-bold">Kunde: </span>
-        <router-link class="underline" :to="`${ROUTES.CLIENT.path}/${orderInfo?.client_id}/edit`">
-          {{ orderInfo?.client.first_name }} {{ orderInfo?.client.last_name }}
-          <i class="pi pi-external-link ml-1"></i>
-        </router-link>
-      </div>
-      <div>
-        <span class="font-bold">Skonto: </span>
-        <span>{{ orderInfo?.can_have_cash_discount ? "Ja" : "Nein" }}</span>
-      </div>
-    </template>
-  </Card>
-  <Card class="my-2">
-    <template #content>
-      <div class="mb-4 font-bold">Rechnung:</div>
-      <FloatLabel class="my-6">
-        <Calendar
-          id="invoice-date-input"
-          v-model="invoiceDate"
-          dateFormat="dd/mm/yy"
-          showIcon
-          iconDisplay="input"
-        />
-        <label for="calendar"> Rechnungsdatum </label>
-      </FloatLabel>
-      <FloatLabel class="my-6">
-        <Calendar
-          id="payment-target-input"
-          v-model="paymentTarget"
-          dateFormat="dd/mm/yy"
-          showIcon
-          iconDisplay="input"
-        />
-        <label for="calendar"> Zalungsziel </label>
-      </FloatLabel>
-      <div class="mb-2">Zahlungstatus:</div>
-      <Dropdown
-        v-model="invoiceInfo.status"
-        :options="invoiceType"
-        placeholder="Anrede"
-        class="w-full md:w-[14rem] mb-3"
-      />
-      <div class="flex flex-row justify-between items-center mb-4">
-        <div class="font-bold">Leistungsdatum:</div>
-        <Button @click="onServiceDateCreate" icon="pi pi-plus" rounded outlined />
-      </div>
-      <div
-        class="flex flex-row justify-between items-center"
-        v-for="(item, idx) in serviceDates"
-        :key="item.id"
-      >
+    <OrderSummary :order-info="orderInfo" />
+    <Card class="my-2">
+      <template #content>
+        <div class="mb-4 font-bold">Rechnung:</div>
         <FloatLabel class="my-6">
           <Calendar
-            :id="item.id.toString()"
-            v-model="item.date"
+            id="invoice-date-input"
+            v-model="invoiceDate"
             dateFormat="dd/mm/yy"
             showIcon
             iconDisplay="input"
           />
-          <label for="calendar"> Leistungsdatum {{ idx + 1 }} </label>
+          <label for="calendar"> Rechnungsdatum </label>
         </FloatLabel>
-        <Button
-          @click="onServiceDateDelete(item.id)"
-          icon="pi pi-times"
-          severity="danger"
-          text
-          raised
-        />
-      </div>
-      <section>
-        <p class="font-bold mb-5">Rechnungsbeschreibung:</p>
-        <FloatLabel>
-          <Textarea
-            id="text"
-            v-model="invoiceInfo.description"
-            class="w-full"
-            autoResize
-            rows="5"
-            cols="30"
+        <FloatLabel class="my-6">
+          <Calendar
+            id="payment-target-input"
+            v-model="paymentTarget"
+            dateFormat="dd/mm/yy"
+            showIcon
+            iconDisplay="input"
           />
-          <label for="text">Beschreibung</label>
+          <label for="calendar"> Zalungsziel </label>
         </FloatLabel>
-        <div class="font-bold">Summe:</div>
-        <div class="flex flex-row gap-10 my-3">
-          <span>Netto: {{ allItemsSum.amountNet }} </span>
-          <span>USt: {{ allItemsSum.amountVat }} </span>
-          <span>Brutto: {{ allItemsSum.amountGross }}</span>
+        <div class="mb-2">Zahlungstatus:</div>
+        <Dropdown
+          v-model="invoiceInfo.status"
+          :options="invoiceType"
+          placeholder="Anrede"
+          class="w-full md:w-[14rem] mb-3"
+        />
+        <div class="flex flex-row justify-between items-center mb-4">
+          <div class="font-bold">Leistungsdatum:</div>
+          <Button @click="onServiceDateCreate" icon="pi pi-plus" rounded outlined />
         </div>
-        <SplitButton label="Hinzufügen" :model="items" :class="'w-full'" />
-      </section>
-    </template>
-  </Card>
-  <SubOrderItem
-    v-for="(item, idx) in invoiceItemsArray"
-    :index="idx + 1"
-    :item="item"
-    :key="item.id"
-    :vat-date="invoiceInfo.invoice_date"
-    @deleted="onItemDelete"
-    @updated="
-      (item) => {
-        onItemUpdate(item);
-      }
-    "
-  ></SubOrderItem>
+        <div
+          class="flex flex-row justify-between items-center"
+          v-for="(item, idx) in serviceDates"
+          :key="item.id"
+        >
+          <FloatLabel class="my-6">
+            <Calendar
+              :id="item.id.toString()"
+              v-model="item.date"
+              dateFormat="dd/mm/yy"
+              showIcon
+              iconDisplay="input"
+            />
+            <label for="calendar"> Leistungsdatum {{ idx + 1 }} </label>
+          </FloatLabel>
+          <Button
+            @click="onServiceDateDelete(item.id)"
+            icon="pi pi-times"
+            severity="danger"
+            text
+            raised
+          />
+        </div>
+        <section>
+          <p class="font-bold mb-5">Rechnungsbeschreibung:</p>
+          <FloatLabel>
+            <Textarea
+              id="text"
+              v-model="invoiceInfo.description"
+              class="w-full"
+              autoResize
+              rows="5"
+              cols="30"
+            />
+            <label for="text">Beschreibung</label>
+          </FloatLabel>
+          <div class="font-bold">Summe:</div>
+          <div class="flex flex-row gap-10 my-3">
+            <span>Netto: {{ allItemsSum.amountNet }} </span>
+            <span>USt: {{ allItemsSum.amountVat }} </span>
+            <span>Brutto: {{ allItemsSum.amountGross }}</span>
+          </div>
+          <SplitButton label="Hinzufügen" :model="items" :class="'w-full'" />
+        </section>
+      </template>
+    </Card>
+    <SubOrderItem
+      v-for="(item, idx) in invoiceItemsArray"
+      :index="idx + 1"
+      :item="item"
+      :key="item.id"
+      :vat-date="invoiceInfo.invoice_date"
+      @deleted="onItemDelete"
+      @updated="
+        (item) => {
+          onItemUpdate(item);
+        }
+      "
+    ></SubOrderItem>
+  </div>
 </template>
