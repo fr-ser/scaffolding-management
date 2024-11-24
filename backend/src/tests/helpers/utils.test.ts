26ab5fd1841013e451b2341b9761be9304b14ed9
Sergej Herbert <sergej.herbert@sennder.com>
2024-11-24 16:44:16 +0100

    fix fix sorting of documents

--- backend/src/tests/helpers/utils.test.ts
+++ backend/src/tests/helpers/utils.test.ts
@@ -5,21 +5,9 @@ import { mergeSortedDocuments } from "@/helpers/utils";
 
 describe("mergeSortedDocuments", () => {
   test("sorting", async () => {
-    const array1 = [
-      { creation_date: "2024-07-01" },
-      { creation_date: "2024-07-04" },
-      { creation_date: "2024-07-07" },
-    ] as OfferDocument[];
-    const array2 = [
-      { creation_date: "2024-07-02" },
-      { creation_date: "2024-07-05" },
-      { creation_date: "2024-07-08" },
-    ] as OfferDocument[];
-    const array3 = [
-      { creation_date: "2024-07-03" },
-      { creation_date: "2024-07-06" },
-      { creation_date: "2024-07-09" },
-    ] as OfferDocument[];
+    const array1 = [{ created_at: 1 }, { created_at: 4 }, { created_at: 7 }] as OfferDocument[];
+    const array2 = [{ created_at: 2 }, { created_at: 5 }, { created_at: 8 }] as OfferDocument[];
+    const array3 = [{ created_at: 3 }, { created_at: 6 }, { created_at: 9 }] as OfferDocument[];
 
     const mergedArray = mergeSortedDocuments(array1, array2, array3, {
       isAscending: true,
@@ -27,28 +15,28 @@ describe("mergeSortedDocuments", () => {
     });
 
     expect(mergedArray).toEqual([
-      { creation_date: "2024-07-01" },
-      { creation_date: "2024-07-02" },
-      { creation_date: "2024-07-03" },
-      { creation_date: "2024-07-04" },
-      { creation_date: "2024-07-05" },
-      { creation_date: "2024-07-06" },
-      { creation_date: "2024-07-07" },
-      { creation_date: "2024-07-08" },
-      { creation_date: "2024-07-09" },
+      { created_at: 1 },
+      { created_at: 2 },
+      { created_at: 3 },
+      { created_at: 4 },
+      { created_at: 5 },
+      { created_at: 6 },
+      { created_at: 7 },
+      { created_at: 8 },
+      { created_at: 9 },
     ]);
   });
 
   test("maxItems", async () => {
-    const array1 = [{ creation_date: "2024-07-01" }] as OfferDocument[];
-    const array2 = [{ creation_date: "2024-07-02" }] as OfferDocument[];
-    const array3 = [{ creation_date: "2024-07-04" }] as OfferDocument[];
+    const array1 = [{ created_at: 1 }] as OfferDocument[];
+    const array2 = [{ created_at: 2 }] as OfferDocument[];
+    const array3 = [{ created_at: 4 }] as OfferDocument[];
 
     const mergedArray = mergeSortedDocuments(array1, array2, array3, {
       isAscending: true,
       maxItems: 2,
     });
 
-    expect(mergedArray).toEqual([{ creation_date: "2024-07-01" }, { creation_date: "2024-07-02" }]);
+    expect(mergedArray).toEqual([{ created_at: 1 }, { created_at: 2 }]);
   });
 });
