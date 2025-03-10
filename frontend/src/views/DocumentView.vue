49f08e47a3b025e3e6c58ce3ef4307d2cac5cacd
Sergej Herbert <sergej.herbert@fake-git.com>
2025-03-10 15:19:44 +0100

    feat: add document menu

--- frontend/src/views/DocumentView.vue
+++ frontend/src/views/DocumentView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
+import Menu from "primevue/menu";
 import { onMounted, ref } from "vue";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 
 import { getInvoiceDocument, getOfferDocument, getOverdueNoticeDocument } from "@/backendClient";
 import DocumentArticleTable from "@/components/documents/DocumentArticleTable.vue";
@@ -8,38 +9,62 @@ import DocumentFooter from "@/components/documents/DocumentFooter.vue";
 import DocumentInvoiceItemTable from "@/components/documents/DocumentInvoiceItemTable.vue";
 import DocumentText from "@/components/documents/DocumentText.vue";
 import DocumentTitle from "@/components/documents/DocumentTitle.vue";
+import { neverFunction } from "@/global/helpers";
 import { DocumentKind } from "@/global/types/appTypes";
 import type {
   InvoiceDocument,
   OfferDocument,
   OverdueNoticeDocument,
 } from "@/global/types/entities";
+import { getOrderEditPath } from "@/helpers/routes";
 
+const router = useRouter();
 const route = useRoute();
-let result = ref<OfferDocument | OverdueNoticeDocument | InvoiceDocument>();
 const kind = route.params.kind as DocumentKind;
+
+let result = ref<OfferDocument | OverdueNoticeDocument | InvoiceDocument>();
+
+const menuItems = ref([
+  {
+    label: "E-Mail-Versand",
+    icon: "pi pi-envelope",
+    command: function () {
+      alert("email");
+    },
+  },
+  {
+    label: "Auftrag anzeigen",
+    icon: "pi pi-shop",
+    command: function () {
+      if (result.value != null) {
+        router.push(getOrderEditPath(result.value.order_id));
+      }
+    },
+  },
+]);
 
 async function getDocument() {
   if (kind === DocumentKind.offer) {
     result.value = await getOfferDocument(route.params.id as string);
     return result;
-  }
-  if (kind === DocumentKind.invoice) {
+  } else if (kind === DocumentKind.invoice) {
     result.value = await getInvoiceDocument(route.params.id as string);
     return result;
-  }
-  if (kind === DocumentKind.overdueNotice) {
+  } else if (kind === DocumentKind.overdueNotice) {
     result.value = await getOverdueNoticeDocument(route.params.id as string);
     return result;
-  }
+  } else neverFunction(kind);
 }
+
 onMounted(async () => {
   await getDocument();
 });
 </script>
 
 <template>
-  <div class="w-full">
+  <div class="w-full flex gap-2 flex-col-reverse lg:flex-row">
+    <Menu :model="menuItems" />
+
     <div
       v-if="result"
       class="min-h-297 w-[60rem] px-[4rem] ml-auto mr-auto py-5 border-solid border-2 border-slate-500 box-border"
