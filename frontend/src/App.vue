0e315ba0402dba91da53327c20291d271cf9b39c
Sergej Herbert <sergej.herbert@sennder.com>
2025-04-09 19:35:32 +0200

    feat: add global error handling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,25 @@
 <script setup lang="ts">
 import ConfirmDialog from "primevue/confirmdialog";
 import Toast from "primevue/toast";
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import { RouterView } from "vue-router";
 
 import NavigationBar from "@/components/NavigationBar.vue";
+import { setGlobalToastRef } from "@/composables/useNotifications";
 import { useUserStore } from "@/store";
 
 const userStore = useUserStore();
+const toast = ref();
 
 onMounted(() => {
   userStore.loadPermissions();
+  setGlobalToastRef(toast);
 });
 </script>
 
 <template>
   <div class="h-screen flex flex-col overflow-hidden w-full">
-    <Toast class="w-3/4 sm:w-max" group="global" />
+    <Toast class="w-3/4 sm:w-max" group="global" ref="toast" />
     <ConfirmDialog class="w-max"></ConfirmDialog>
     <NavigationBar />
     <div class="px-3 py-3 bg-neutral-100 grow overflow-auto">
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,3 +1,4 @@
+import { showGlobalErrorToast } from "./composables/useNotifications";
 import { createPinia } from "pinia";
 import PrimeVue from "primevue/config";
 import ConfirmationService from "primevue/confirmationservice";
@@ -17,5 +18,16 @@ app.use(PrimeVue, { unstyled: true, pt: Lara });
 
 app.use(createPinia());
 app.use(router);
+
+function errorHandler(err: unknown) {
+  showGlobalErrorToast("Ein Fehler ist aufgetreten: " + String(err));
+}
+
+app.config.errorHandler = errorHandler;
+
+// rejected promises do not go to Vue and need to be caught like this
+window.addEventListener("unhandledrejection", (event) => {
+  errorHandler(event.reason);
+});
 
 app.mount("#app");
