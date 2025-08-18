4c7dd6b0a3642263d5d4c13cd1191bcc62cd19f2
Sergej Herbert <sergej.herbert@sennder.com>
2025-08-18 18:39:07 +0200

    feat: better error logging

--- backend/src/helpers/logging.ts
+++ backend/src/helpers/logging.ts
@@ -1,6 +1,13 @@
 export function log(message: string, error?: Error) {
   if (error) {
-    console.log(new Date().toISOString(), "Error:", message, JSON.stringify(error));
+    console.log(
+      new Date().toISOString(),
+      "Error:",
+      message,
+      `Error message: ${error?.message || "No message"}`,
+      JSON.stringify(error),
+      `Error stack: ${error?.stack || "No stack trace"}`,
+    );
   } else {
     console.log(new Date().toISOString(), message);
   }
