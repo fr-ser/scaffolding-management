52098c7936b83b3ebf148673da30424cd0c345e0
Sergej Herbert <sergej.herbert@sennder.com>
2025-08-22 22:49:21 +0200

    feat: upgrade primevue

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,5 @@
 import { showGlobalErrorToast } from "./composables/useNotifications";
+import Lara from "@primeuix/themes/lara";
 import { createPinia } from "pinia";
 import PrimeVue from "primevue/config";
 import ConfirmationService from "primevue/confirmationservice";
@@ -7,14 +8,13 @@ import { createApp } from "vue";
 
 import App from "@/App.vue";
 import "@/assets/main.css";
-import Lara from "@/presets/lara";
 import router from "@/router";
 
 const app = createApp(App);
 app.use(ConfirmationService);
 app.use(ToastService);
 
-app.use(PrimeVue, { unstyled: true, pt: Lara });
+app.use(PrimeVue, { theme: { preset: Lara } });
 
 app.use(createPinia());
 app.use(router);
